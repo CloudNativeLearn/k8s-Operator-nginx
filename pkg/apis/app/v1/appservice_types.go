582dff1e1c403c69fab4931114f4d9b7c994e65d
方玉龙 <3095329264@qq.com>
2020-08-04 18:06:55 +0800

    第一次提交

---
+++ pkg/apis/app/v1/appservice_types.go
@@ -0,0 +1,61 @@
+package v1
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	//appv1 "github.com/cnych/opdemo/pkg/apis/app/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//corev1 "k8s.io/api/core/v1"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AppServiceSpec defines the desired state of AppService
+type AppServiceSpec struct {
+	Size  	  *int32                      `json:"size"`
+	Image     string                      `json:"image"`
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
+	Envs      []corev1.EnvVar             `json:"envs,omitempty"`
+	Ports     []corev1.ServicePort        `json:"ports,omitempty"`
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// AppServiceStatus defines the observed state of AppService
+type AppServiceStatus struct {
+	appsv1.DeploymentStatus `json:",inline"`
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AppService is the Schema for the appservices API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=appservices,scope=Namespaced
+type AppService struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AppServiceSpec   `json:"spec,omitempty"`
+	Status AppServiceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AppServiceList contains a list of AppService
+type AppServiceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []AppService `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&AppService{}, &AppServiceList{})
+}
